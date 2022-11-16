9aacd5cacfbf939c0eb5eb79524afdd1f184c2db
William Taylor <willtaylor147@gmail.com>
2022-11-16 16:15:41 +0000

    Yelp-MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+#   # root "articles#index"
+#   get '/reviews/new', to: 'reviews#new'
+#   post '/reviews', to: 'reviews#create'
+
+#   get '/restaurants', to: 'restaurants#index'
+#   get '/restaurants/new', to: 'restaurants#new'
+#   post '/restaurants', to: 'restaurants#create'
+#   get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating Restaurants'
+
+hare_and_tortoise = { name: 'Hare and Tortoise', address: '11-13 Brunswick Square, London WC1N 1AF', category: 'japanese' }
+morleys = { name: 'Morleys', address: '48a Church St, Enfield EN2 6AZ', category: 'french' }
+pizza_express = { name: 'Pizza Express', address: '1 Clerkenwell Rd, London EC1M 5PA', category: 'italian' }
+ikeda = { name: 'Ikeda', address: '30 Brook Street, London W1K 5DJ', category: 'japanese' }
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'belgian'}
+
+[hare_and_tortoise, morleys, pizza_express, ikeda, dishoom].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
